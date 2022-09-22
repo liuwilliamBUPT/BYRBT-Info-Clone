f2a8770fbceff32b6149e365b03ea1c623c98867
William <liuwilliam.zh@gmail.com>
2022-09-23 00:17:18 +0800

    chore(vscode): Add vscode setings

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  rules: {
+    'no-use-before-define': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^h$',
+        varsIgnorePattern: '^h$',
+      },
+    ],
+    'space-before-function-paren': 'off',
+		'curly': 'all',
+  },
+}
